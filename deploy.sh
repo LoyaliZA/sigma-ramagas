abd058f58b1df84342fa4e98a46bbb0b25f9808f
Gabriel <contacto.loyal1a@gmail.com>
2026-01-10 17:41:58 +0000

    Actualización deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "Iniciando despliegue de SIGMA..."
 
-# 1. Bajar ultimos cambios de git
+# 1. Bajar codigo
 git pull origin main
 
-# 2. Construir y levantar contenedores en segundo plano
-docker compose -f docker-compose.prod.yml up -d --build
+# 2. Construir y levantar (Nota el guion en docker-compose)
+docker-compose -f docker-compose.prod.yml up -d --build
 
-# 3. Mantenimiento de Laravel (Migraciones y Cache)
-docker compose -f docker-compose.prod.yml exec -T app php artisan migrate --force
-docker compose -f docker-compose.prod.yml exec -T app php artisan config:cache
-docker compose -f docker-compose.prod.yml exec -T app php artisan route:cache
-docker compose -f docker-compose.prod.yml exec -T app php artisan view:cache
+# 3. Mantenimiento
+docker-compose -f docker-compose.prod.yml exec -T app php artisan migrate --force
+docker-compose -f docker-compose.prod.yml exec -T app php artisan storage:link
+docker-compose -f docker-compose.prod.yml exec app composer dump-autoload
+docker-compose -f docker-compose.prod.yml exec -T app php artisan optimize:clear
+docker-compose -f docker-compose.prod.yml exec -T app php artisan config:cache
+docker-compose -f docker-compose.prod.yml exec -T app php artisan route:cache
+docker-compose -f docker-compose.prod.yml exec -T app php artisan view:cache
+
 
 echo "Despliegue finalizado exitosamente."
